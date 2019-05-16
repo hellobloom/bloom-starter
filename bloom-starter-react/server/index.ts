61662f76212798ca45354ee4c3718de15d6b7fe6
Isaac Patka <ipatka@gmail.com>
2019-05-16 11:59:50 -0400

    Support blip4 (#6)
    support Verifiable Credential spec BLIP4

--- bloom-starter-react/server/index.ts
+++ bloom-starter-react/server/index.ts
@@ -1,154 +1,154 @@
-import express from "express";
-import * as bodyParser from "body-parser";
-import helmet from "helmet";
-import compress from "compression";
-import session from "express-session";
-import uuid from "uuid";
-import path from "path";
-import http from "http";
-import morgan from "morgan";
-import { validateResponseData } from "@bloomprotocol/share-kit";
+import express from 'express'
+import * as bodyParser from 'body-parser'
+import helmet from 'helmet'
+import compress from 'compression'
+import session from 'express-session'
+import uuid from 'uuid'
+import path from 'path'
+import http from 'http'
+import morgan from 'morgan'
+import { validateUntypedResponseData } from '@bloomprotocol/share-kit'
 
-import { loggedInSession } from "./middleware";
-import { applySocket, sendSocketMessage } from "./socket";
-import { env } from "./environment";
+import { loggedInSession } from './middleware'
+import { applySocket, sendSocketMessage } from './socket'
+import { env } from './environment'
 
 /**
  * WARNING: This "database" is NOT intended to be used in production applications.
  */
-const database: { [key: string]: string } = {};
+const database: { [key: string]: string } = {}
 
 const sessionParser = session({
   saveUninitialized: false,
   secret: env.sessionSecret,
   resave: false
-});
+})
 
-const app = express();
+const app = express()
 
-app.use(helmet());
-app.use(morgan("tiny"));
+app.use(helmet())
+app.use(morgan('tiny'))
 
-app.use(sessionParser);
+app.use(sessionParser)
 
 app.use(
   bodyParser.json({
-    type: "*/*",
+    type: '*/*',
     verify: (req, _, buf) => {
-      (req as any).rawBody = buf;
-      return true;
+      ;(req as any).rawBody = buf
+      return true
     },
-    limit: "10mb" // https://stackoverflow.com/a/19965089/1165441
+    limit: '10mb' // https://stackoverflow.com/a/19965089/1165441
   })
-);
+)
 
-app.use(compress());
+app.use(compress())
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, 'client')))
 
-app.post("/session", (req, res) => {
+app.post('/session', (req, res) => {
   if (req.session!.userId === undefined) {
-    const id = uuid.v4();
-    req.session!.userId = id;
+    const id = uuid.v4()
+    req.session!.userId = id
   }
 
   return res.status(200).json({
     success: true,
-    message: "Session updated",
+    message: 'Session updated',
     token: req.session!.userId
-  });
-});
+  })
+})
 
-app.delete("/clear-session", loggedInSession, (req, res) => {
+app.delete('/clear-session', loggedInSession, (req, res) => {
   if (req.session) {
     req.session.destroy(err => {
       if (err) {
         res.status(500).send({
           success: false,
-          message: "Something went wrong while destroying session"
-        });
+          message: 'Something went wrong while destroying session'
+        })
       } else {
-        res.send({ success: true, message: "Session destroyed" });
+        res.send({ success: true, message: 'Session destroyed' })
       }
-    });
+    })
   }
-});
+})
 
-app.post("/scan", async (req, res) => {
+app.post('/scan', async (req, res) => {
   try {
-    const verifiedData = await validateResponseData(req.body, {
+    const verifiedData = await validateUntypedResponseData(req.body, {
       validateOnChain: env.validateOnChain,
       web3Provider: env.web3Provider
-    });
-    if (verifiedData.errors.length) {
+    })
+    if (verifiedData.kind === 'invalid') {
       res.status(400).json({
         success: false,
-        message: "Shared data is not valid",
+        message: 'Shared data is not valid',
         verifiedData
-      });
-      return;
+      })
+      return
+    }
+    const consumableEmailData = verifiedData.data.verifiableCredential.find(
+      data => data.type === 'email'
+    )
+    const email =
+      consumableEmailData && consumableEmailData.credentialSubject.data
+    if (!email || email.trim() === '') {
+      throw new Error('Missing email')
     }
 
-    const consumableEmailData = verifiedData.data.find(
-      data => data.type === "email"
-    );
-    const email = consumableEmailData && consumableEmailData.data;
-    if (!email || email.trim() === "") {
-      throw new Error("Missing email");
-    }
-
-    const sharePayload = JSON.stringify({ email });
-    if (req.query["share-kit-from"] === "button") {
-      database[req.body.token] = sharePayload;
+    const sharePayload = JSON.stringify({ email })
+    if (req.query['share-kit-from'] === 'button') {
+      database[req.body.token] = sharePayload
     } else {
       await sendSocketMessage({
         userId: req.body.token,
-        type: "share-kit-scan",
+        type: 'share-kit-scan',
         payload: sharePayload
-      });
+      })
     }
 
-    res.status(200).json({ success: true, message: "Message Sent" });
+    res.status(200).json({ success: true, message: 'Message Sent' })
   } catch (err) {
-    if (err.message === "Missing email") {
+    if (err.message === 'Missing email') {
       res.status(404).send({
         success: false,
-        message: "Email is missing from completed attestations"
-      });
+        message: 'Email is missing from completed attestations'
+      })
     } else {
       res.status(500).send({
         success: false,
-        message: "Something went wrong while sending message"
-      });
+        message: 'Something went wrong while sending message'
+      })
     }
   }
-});
+})
 
-app.get("/received-data", async (req, res) => {
-  const receivedData = database[req.query.token];
+app.get('/received-data', async (req, res) => {
+  const receivedData = database[req.query.token]
   if (receivedData) {
     res.status(200).json({
       success: true,
       receivedData: JSON.parse(receivedData)
-    });
-    delete database[req.query.token];
-    return;
+    })
+    delete database[req.query.token]
+    return
   }
 
   return res.status(400).json({
     success: false,
-    message: "Something went wrong while attempting to query for received data"
-  });
-});
+    message: 'Something went wrong while attempting to query for received data'
+  })
+})
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
-});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'))
+})
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-applySocket(server, sessionParser);
+applySocket(server, sessionParser)
 
 server.listen(env.port, () =>
   console.log(`Listening on http://localhost:${env.port}`)
-);
+)
