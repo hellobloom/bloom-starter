81d007d52450172ca8c7b0683d74645040f45ec4
djvs <djvansch@gmail.com>
2019-11-13 20:51:34 -0500

    fix

--- bloom-starter-auth/server/socket.ts
+++ bloom-starter-auth/server/socket.ts
@@ -1,30 +1,31 @@
-import express from "express";
-import http from "http";
-import WebSocket from "ws";
+import express from 'express'
+import http from 'http'
+import WebSocket from 'ws'
 
 type WebSocketMap = {
-  [userId: string]: WebSocket | undefined;
-};
+  [userId: string]: WebSocket | undefined
+}
 
-const webSockets: WebSocketMap = {};
+const webSockets: WebSocketMap = {}
 
 export const sendSocketMessage = (message: {
-  userId: string;
-  type: string;
-  payload: string;
+  userId: string
+  type: string
+  payload: string
 }) => {
-  const socket = webSockets[message.userId];
+  const socket = webSockets[message.userId]
+  console.log('Sending msg to socket', socket, message)
 
-  if (socket === undefined) return;
+  if (socket === undefined) return
 
   return new Promise((resolve, reject) => {
     socket.send(JSON.stringify([message.type, message.payload]), err => {
-      if (err) reject(err);
+      if (err) reject(err)
 
-      resolve();
-    });
-  });
-};
+      resolve()
+    })
+  })
+}
 
 export const applySocket = (
   server: http.Server,
@@ -32,15 +33,15 @@ export const applySocket = (
 ) => {
   const wss = new WebSocket.Server({
     verifyClient: (info, done) => {
-      console.log("Parsing session from request...");
+      console.log('Parsing session from request...')
       sessionParser(info.req as any, {} as any, () => {
-        done((info.req as any).session.userId !== undefined);
-      });
+        done((info.req as any).session.userId !== undefined)
+      })
     },
-    server
-  });
+    server,
+  })
 
-  wss.on("connection", (ws, req) => {
-    webSockets[(req as any).session.userId] = ws;
-  });
-};
+  wss.on('connection', (ws, req) => {
+    webSockets[(req as any).session.userId] = ws
+  })
+}
